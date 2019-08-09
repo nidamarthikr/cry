89f318125debd6248074d718cebc1bdd417eabfe
Rita Giordano <ritagiordano@Ritas-MacBook-Pro.local>
2019-08-09 16:05:45 +0100

    plotSHELXC.R: add .plotSHELXC

--- R/plotSHELXC.R
+++ R/plotSHELXC.R
@@ -21,6 +21,31 @@
 
 plotSHELXC <-function(filenameDF, var, message = TRUE)
 {
-  plot <- .plotSHELXC(filenameDF, var, message = TRUE)
-  return(plot)
+    gg <- ggplot(filenameDF, aes(1/(Res)^2, var))
+    if(var == filenameDF$d_sig && length(var) >= 1) {
+      gp <- gg + geom_point() + geom_line() + theme_bw() +
+        xlab(expression(h^2 * (ring(A)^-2))) + ylab(expression(Delta*F/sig(Delta*F)))
+    }
+    if(var == filenameDF$Chi_sq && length(var) >= 1){
+      gp <- gg + geom_point() + geom_line() + theme_bw() +
+        xlab(expression(h^2 * (ring(A)^-2))) + ylab(expression(Chi^2))
+    }
+    if(var == filenameDF$I_sig && length(var) >= 1){
+      gp <- gg + geom_point() + geom_line() + theme_bw() +
+        xlab(expression(h^2 * (ring(A)^-2))) + ylab(expression(I/sig(I)))
+    }
+    if(var == filenameDF$Complete && length(var) >= 1) {
+      gp <- gg + geom_point() + geom_line() + theme_bw() +
+        xlab(expression(h^2 * (ring(A)^-2))) + ylab(expression(Completeness))
+    }
+    if(var == filenameDF$CC1_2 && length(var) >= 1){
+      gp <- gg + geom_point() + geom_line() + theme_bw() +
+        xlab(expression(h^2 * (ring(A)^-2))) + ylab(expression(CC_(1/2)))
+    }
+    # gg + geom_point() + geom_line() + theme_bw() +
+    #   xlab(expression(h^2 * (A^-2))) + ylab(expression(Delta*F/sig(Delta*F)))
+
+    return(gp)
+
+
 }
