2366b1f982ab23707ba4f301afa574602a21bc41
nidamarthikr <kuttu.nani@gmail.com>
2020-11-19 19:47:49 +0900

    A prototype of readcif function for single crystal diffraction data.

--- R/cif.R
+++ R/cif.R
@@ -1,7 +1,24 @@
 #
 # This file is part of the cry package
 #
-readcif <- function(filename, messages=FALSE){
+# Functions connected to reflections data.
+
+#' Reads and output an CIF file
+#'
+#' @param filename A character string. The path to a valid CIF file.
+#' @param messages A logical variable. If TRUE (default) the function prints
+#'    a message highlighting what is included in the cif file.
+#' @return A named list. Each name correspond to a valid field in the cif.
+#' @examples
+#' filename <- "/path_to_file/example.cif"
+#' lcif <- readCIF(filename)
+#' print(names(lcif))
+#' print(lcif$INTRO$CELL)
+#' print(lcif$INTRO$HALL)
+#' print(lcif$INTRO$HM)
+#' print(lcif$SYMM)
+#' @export
+readCIF <- function(filename, messages=FALSE){
   f <- file(filename)
   lcif <- readLines(f,warn=FALSE)
   l_list <- grep("loop_",lcif)
@@ -11,26 +28,34 @@ readcif <- function(filename, messages=FALSE){
   crystal_summary <- lapply(ch, ucheck, pattern="_publ_author_name")
   symmetry <- lapply(ch, ucheck, pattern="_space_group_symop_operation")
   reflection <- lapply(ch, ucheck, pattern="_refln.index_h")
-  coordinates <- lapply(ch, ucheck, pattern="_atom_site_label")
+  coordinates <- lapply(ch, ucheck, pattern="_atom_site_fract_x")
   anisotropy <- lapply(ch, ucheck, pattern="_atom_site_aniso_label")
   symbol <- lapply(ch, ucheck, pattern="_atom_type_symbol")
-  #geom_angle <- lapply(ch, ucheck, pattern="geom_angle_atom_site_label_1")
-  #geom_distance <- lapply(ch, ucheck, pattern="_geom_bond_atom_site_label_1")
-  #geom_hbond <- lapply(ch, ucheck, pattern="geom_hbond_atom_site_label_D")
-  #geom_torsion <- lapply(ch, ucheck, pattern="geom_torsion_atom_site_label_1")
+  geom_angle <- lapply(ch, ucheck, pattern="geom_angle_atom_site_label_1")
+  geom_distance <- lapply(ch, ucheck, pattern="_geom_bond_atom_site_label_1")
+  geom_hbond <- lapply(ch, ucheck, pattern="geom_hbond_atom_site_label_D")
+  geom_torsion <- lapply(ch, ucheck, pattern="geom_torsion_atom_site_label_1")
 
   intro <- r_summ(lcif)
-  symm <- nanona(symmetry)
-  reflections <- nanona(reflection)
-  coordinate <- r_positions(nanona(coordinates))
-  anisotropies <- r_aniso(nanona(anisotropy))
-  symbolics <- nanona(symbol)
-  #angle <- r_angle(geom_angle)
-  #distance <- r_dist(geom_distance)
-  #hbond <- r_hbond(geom_hbond)
-  #torsion <- r_torsion(geom_torsion)
-  CIF=list(INTRO=intro,SYMM=symm,COOR=coordinate,ANISO=anisotropies)
-  #CIF = list(INTRO=intro,SYMM=symm,REFL=reflections,COOR=coordinate,ANISO=anisotropies,SYMB=symbolics,ANGLE=angle,DIST=distance,HBOND=hbond,TORSION=torsion)
+  symm <- ansnull(symmetry)
+  #reflections <- if (is.na(nanona(reflection)) == FALSE) r_angle(reflection) else NULL
+  reflections <- ansnull(reflection)
+  coordinate <- if (is.na(nanona(coordinates)) == FALSE) r_positions(nanona(coordinates)) else NULL
+  #coordinate <- r_positions(ansnull(coordinates))
+  anisotropies <- if (is.na(nanona(anisotropy)) == FALSE) r_aniso(nanona(anisotropy)) else NULL
+  #anisotropies <- r_aniso(ansnull(anisotropy))
+  symbolics <- ansnull(symbol)
+  angle <- if (is.na(nanona(geom_angle)) == FALSE) r_angle(nanona(geom_angle)) else NULL
+  #angle <- r_angle(ansnull(geom_angle))
+  distance <- if (is.na(nanona(geom_distance)) == FALSE) r_dist(nanona(geom_distance)) else NULL
+  #distance <- r_dist(ansnull(geom_distance))
+  hbond <- if (is.na(nanona(geom_hbond)) == FALSE) r_hbond(nanona(geom_hbond)) else NULL
+  #hbond <- r_hbond(ansnull(geom_hbond))
+  torsion <- if (is.na(nanona(geom_torsion)) == FALSE) r_torsion(nanona(geom_torsion)) else NULL
+  #torsion <- r_torsion(ansnull(geom_torsion))
+  #CIF=list(INTRO=intro,SYMM=symm,COOR=coordinate,ANISO=anisotropies)
+  CIF = list(HEADER=intro,SYMM=symm,REFL=reflections,COOR=coordinate,ANISO=anisotropies,SYMB=symbolics,ANGLE=angle,DIST=distance,HBOND=hbond,TORSION=torsion)
+  close(f)
   return(CIF)
 }
 
@@ -52,6 +77,15 @@ ucheck <- function(x,pattern){
   }
 }
 
+ansnull <- function(x){
+  if (all(is.na(x)) == TRUE){
+    out <- NULL
+  } else
+  { out <- x[!is.na(x)]
+  return(out)
+  }
+}
+
 nanona <- function(x){
   if (all(is.na(x)) == TRUE){
     out <- NA
@@ -61,10 +95,17 @@ nanona <- function(x){
   }
 }
 
+recheck <- function(r1){
+  r2 <- gsub("[:):]","",gsub("[:(:]",",",r1))
+  r3 <- as.numeric((strsplit(r2, ",")[[1]])[1])
+  return(r3)
+}
+
 check <- function(pattern,word){
   r <- grep(pattern, word, value = TRUE)
   r1 <- if(length(r) > 0) (strsplit(r, "\\s+")[[1]])[2] else NA
-  return(r1)
+  r2 <- if (length(grep("[:(:]",r1,value = TRUE)>0) == TRUE) recheck(r1) else r1
+  return(r2)
 }
 
 check1 <- function(pattern,word){
@@ -75,58 +116,77 @@ check1 <- function(pattern,word){
 
 r_positions <- function (x){
   data <- unlist(x)
+  nskip <- length((grep("_atom",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=4,col.names=c("label","x","y","z")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_atom",data,value=TRUE))
+  colnames(res) <- c(gsub("_atom_site_","",l_l))
   return(res)
 }
 
 r_aniso <- function(x){
   data <- unlist(x)
+  nskip <- length((grep("_atom",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=7,col.names=c("label","U_11","U_22","U_33","U_12","U_13","U_23")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_atom",data,value=TRUE))
+  colnames(res) <- c(gsub("_atom_site_aniso_","",l_l))
   return(res)
 }
 
 
 r_angle <- function(x){
   data <- unlist(x)
+  nskip <- length((grep("_geom_angle",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=4,col.names=c("label_1","label_2","label_3","angle")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_geom_angle",data,value=TRUE))
+  colnames(res) <- c(gsub("_geom_angle_","",l_l))
   return(res)
 }
 
 r_dist <- function(x){
   data <- unlist(x)
+  nskip <- length((grep("_geom_bond",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=3,col.names=c("label_1","label_2","distance")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_geom_bond",data,value=TRUE))
+  colnames(res) <- c(gsub("_geom_bond_","",l_l))
   return(res)
 }
 
 r_hbond <- function(x){
   data <- unlist(x)
+  nskip <- length((grep("_geom_hbond",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=8,col.names=c("D","H","A","symmetry_A","DH","HA","DA","DHA")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_geom_hbond",data,value=TRUE))
+  colnames(res) <- c(gsub("_geom_hbond_","",l_l))
   return(res)
 }
 
 r_torsion <- function(x){
   data <- unlist(x)
+  nskip <- length((grep("_geom_torsion",data)))
   lst <- lapply(split(data, cumsum(grepl("^V", data))),
-                function(x) read.table(text=x,skip=5,col.names=c("label_1","label_2","label_3","label_4","torsion")))
+                function(x) read.table(text=x,skip=nskip))
   names(lst) <- NULL
   res <- do.call(`cbind`, lst)
+  l_l <- c(grep("_geom_torsion",data,value=TRUE))
+  colnames(res) <- c(gsub("_geom_torsion_","",l_l))
   return(res)
 }
+
 
 
 r_summ <- function(x){
@@ -138,16 +198,16 @@ r_summ <- function(x){
   al <- check("_cell_angle_alpha",data)
   be <- check("_cell_angle_beta",data)
   ga <- check("_cell_angle_gamma",data)
-  v <- check("_cell_volume",data)
-  den <-check("_exptl_crystal_density_diffrn",data)
+  v <- as.numeric(check("_cell_volume",data))
+  den <-as.numeric(check("_exptl_crystal_density_diffrn",data))
   c_sy <- check("_symmetry_cell_setting",data)
-  sg_n <- check("_space_group_IT_number",data)
+  sg_n <- as.numeric(check("_space_group_IT_number",data))
   sg_hall<- check1("_symmetry_space_group_name_Hall",data)
   sg_HM <- check1("_symmetry_space_group_name_H-M",data)
   cell <- as.numeric(list(a,b,c,al,be,ga))
-  sg <- list(c_sy,sg_n,sg_hall,sg_HM)
-  prop <- list(v,den)
-  summ_c <- list(CELL=cell,SG=sg,PROP=prop)
+  #sg <- list(c_sy,sg_n,sg_hall,sg_HM)
+  #prop <- list(v,den)
+  summ_c <- list(FORMULA=formula,CELL=cell,VOL=v,DEN=den,CrysSys=c_sy,SGN=sg_n,HALL=sg_hall,HM=sg_HM)
   return(summ_c)
 }
 
